4fa059ad955243acd38a54861219c4df5c9df200
Chandu <cchhaanndduu@gmail.com>
2015-12-01 00:10:37 -0500

    + Add small example for Spring MVC Controller

--- src/main/java/com/example/springmvc/Home.java
+++ src/main/java/com/example/springmvc/Home.java
@@ -1,5 +1,32 @@
 package com.example.springmvc;
 
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class Home {
 
+	@RequestMapping("home")
+	public ModelAndView get() {
+		final ModelAndView mv = new ModelAndView("homepage");
+		final ModelMap model = new ModelMap();
+		model.put("msg", "Hello World!");
+		model.put("dt", "Today is " + new Date());
+		mv.addAllObjects(model);
+		return mv;
+	}
+
+	@RequestMapping("info")
+	public ModelAndView get2() {
+		final ModelAndView mv = new ModelAndView("homepage");
+		final ModelMap model = new ModelMap();
+		model.put("msg", "Hi there!");
+		model.put("dt", "Today is " + new Date());
+		mv.addAllObjects(model);
+		return mv;
+	}
 }
